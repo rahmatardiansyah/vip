c3f98131e9da7f5f2c4f840abd7f52b248e67cef
Rahmat Ardiansyah <rahmatardiansyah147@gmail.com>
2023-07-29 17:14:38 +0700

    update

--- static/js/robert.js
+++ static/js/robert.js
@@ -5,11 +5,30 @@ clearFileEl.addEventListener('click', function () {
   inputFileEl.value = '';
 });
 
+// disable result button
+inputFileEl.addEventListener('change', function () {
+  resultButtonEl.setAttribute('disabled', '');
+  processButtonEl.removeAttribute('disabled');
+});
+
 // clear uploaded file if radio clicked
 const radioEl = document.querySelectorAll('.radio-choose-image');
 radioEl.forEach(i => {
-  i.addEventListener('click', () => (inputFileEl.value = ''));
+  i.addEventListener('click', () => {
+    inputFileEl.value = '';
+    resultButtonEl.setAttribute('disabled', '');
+    processButtonEl.removeAttribute('disabled');
+  });
 });
+
+const processButtonEl = document.querySelector('.process');
+processButtonEl.addEventListener('click', process);
+
+const resultButtonEl = document.querySelector('.show-result');
+resultButtonEl.addEventListener('click', showResult);
+
+// get container result
+const resultContainer = document.querySelector('#main .result');
 
 function getImage() {
   if (inputFileEl.value == '') {
@@ -38,217 +57,72 @@ function resizeImage(img, newWidth, newHeight) {
   return canvas;
 }
 
-const img = new Image();
+function rgbToGrayscale(pixelData) {
+  const grayscaleArray = [];
+  for (let i = 0; i < pixelData.length; i += 4) {
+    const grayscale = (pixelData[i] + pixelData[i + 1] + pixelData[i + 2]) / 3;
+    grayscaleArray.push(Math.floor(grayscale));
+  }
+  return grayscaleArray;
+}
 
-function handleImage() {
-  const file = inputFileEl.files[0];
-  const reader = new FileReader();
-  reader.onload = function () {
-    img.onload = function () {
-      const newWidth = 7;
-      const newHeight = 7;
+function getImageDataPromise() {
+  return new Promise((resolve, reject) => {
+    const file = inputFileEl.files[0];
+    const reader = new FileReader();
+    const img = new Image();
+    reader.onload = function () {
+      img.onload = function () {
+        const canvasProses = document.createElement('canvas');
 
-      const resizedImg = resizeImage(img, newWidth, newHeight);
+        const resizedImg = resizeImage(img, 7, 7);
+        canvasProses.style.display = 'none';
+        canvasProses.width = resizedImg.width;
+        canvasProses.height = resizedImg.height;
 
-      const canvas = document.createElement('canvas');
+        const ctx = canvasProses.getContext('2d');
+        ctx.drawImage(resizedImg, 0, 0);
 
-      canvas.style.display = 'none';
-      canvas.width = resizedImg.width;
-      canvas.height = resizedImg.height;
-
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(resizedImg, 0, 0);
-
-      const imageData = ctx.getImageData(
-        0,
-        0,
-        resizedImg.width,
-        resizedImg.height
-      );
-      const pixelData = imageData.data;
-
-      const grayscaleArray = [];
-
-      for (let i = 0; i < pixelData.length; i += 4) {
-        let grayscale =
-          (pixelData[i] + pixelData[i + 1] + pixelData[i + 2]) / 3;
-        pixelData[i] = grayscale;
-        pixelData[i + 1] = grayscale;
-        pixelData[i + 2] = grayscale;
-        grayscaleArray.push(Math.floor(grayscale));
+        const imageData = ctx.getImageData(
+          0,
+          0,
+          resizedImg.width,
+          resizedImg.height
+        ).data;
+        resolve(imageData);
+      };
+      img.onerror = function () {
+        reject('Image Error');
+      };
+      if (file) {
+        img.src = reader.result;
+      } else {
+        img.src = Array.from(document.getElementsByName('image')).find(
+          r => r.checked
+        ).nextElementSibling.src;
       }
-      const tableGrayscaleEl = document.querySelector('.table-grayscale');
-
-      tableGrayscaleEl.innerHTML = '';
-
-      // Hitung jumlah baris dan kolom tabel
-      const numRows = 7;
-      const numCols = 7;
-
-      let index = 0;
-      for (let i = 0; i < numRows; i++) {
-        let row = tableGrayscaleEl.insertRow();
-        for (let j = 0; j < numCols; j++) {
-          let cell = row.insertCell();
-          cell.textContent = grayscaleArray[index];
-          index++;
-        }
-      }
-
-      ctx.putImageData(imageData, 0, 0);
-      const robertProcessEl = document.querySelector('.robert-process');
-      robertProcessEl.style.display = 'block';
-
-      // Show canvas
-      // const canvasEl = document.querySelectorAll('.result canvas');
-      // canvasEl.forEach(i => {
-      //   i.style.display = 'block';
-      // });
     };
     if (file) {
-      img.src = reader.result;
+      reader.readAsDataURL(file);
     } else {
-      img.src = Array.from(document.getElementsByName('image')).find(
-        r => r.checked
-      ).nextElementSibling.src;
+      reader.readAsDataURL(new Blob());
     }
-  };
-  if (file) {
-    reader.readAsDataURL(file);
-  } else {
-    reader.readAsDataURL(new Blob());
-  }
+  });
 }
 
-const processButtonEl = document.querySelector('.process');
+async function processImageData() {
+  try {
+    const imageData = await getImageDataPromise();
 
-processButtonEl.addEventListener('click', test);
+    const grayscaleArray = [...rgbToGrayscale(imageData)];
 
-function test() {
-  handleImage();
-}
-
-function process() {
-  hideCollapse();
-
-  const image = new Image();
-  image.src = getImage();
-  // Menentukan ukuran gambar minimum dan maksimum
-  const minSize = 100;
-  const maxSize = 900;
-
-  image.onload = function () {
-    const canvasOriginal = document.getElementById('canvasOriginal');
-    const canvasGrayscale = document.getElementById('canvasGreyScale');
-    const canvasResult = document.getElementById('canvasResult');
-
-    const canvasProcess = document.getElementById('hidden');
-
-    const contextOriginal = canvasOriginal.getContext('2d');
-    const contextGreyscale = canvasGrayscale.getContext('2d');
-    const contextResult = canvasResult.getContext('2d');
-
-    const contextProcess = canvasProcess.getContext('2d');
-
-    const ratio = image.width / image.height;
-    let width = Math.min(maxSize, Math.max(minSize, canvasOriginal.width));
-    let height = width / ratio;
-
-    if (height > maxSize) {
-      height = maxSize;
-      width = height * ratio;
-    }
-
-    canvasOriginal.width = width;
-    canvasOriginal.height = height;
-
-    contextOriginal.drawImage(image, 0, 0, width, height);
-
-    // RBG to Grayscale
-    const imageDataGreyScale = contextOriginal.getImageData(
-      0,
-      0,
-      width,
-      height
-    );
-    const pixels = imageDataGreyScale.data;
-
-    for (let i = 0; i < pixels.length; i += 4) {
-      let grayscale = (pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3;
-      pixels[i] = grayscale;
-      pixels[i + 1] = grayscale;
-      pixels[i + 2] = grayscale;
-    }
-    canvasGrayscale.width = width;
-    canvasGrayscale.height = height;
-    contextGreyscale.putImageData(imageDataGreyScale, 0, 0);
-
-    // Robert
-    const imageDataRobert = contextGreyscale.getImageData(0, 0, width, height);
-    const pixelsRobert = imageDataRobert.data;
-    const output = new Uint8ClampedArray(pixelsRobert.length);
-
-    const kernelGx = [1, 0, 0, -1];
-    const kernelGy = [0, 1, -1, 0];
-
-    for (let i = 0; i < pixelsRobert.length; i += 4) {
-      let gx = 0;
-      let gy = 0;
-
-      for (let j = 0; j < 4; j++) {
-        let x = (i / 4) % canvasOriginal.width;
-        let y = Math.floor(i / 4 / canvasOriginal.width);
-        let index =
-          ((y + Math.floor(j / 2)) * canvasOriginal.width + (x + (j % 2))) * 4;
-
-        gx += pixelsRobert[index] * kernelGx[j];
-        gy += pixelsRobert[index] * kernelGy[j];
-      }
-
-      let magnitude = Math.sqrt(gx * gx + gy * gy);
-      output[i] = magnitude;
-      output[i + 1] = magnitude;
-      output[i + 2] = magnitude;
-      output[i + 3] = 255;
-    }
-
-    canvasResult.width = width;
-    canvasResult.height = height;
-
-    imageDataRobert.data.set(output);
-    contextResult.putImageData(imageDataRobert, 0, 0);
-
-    const robertProcessEl = document.querySelector('.robert-process');
-    robertProcessEl.style.display = 'block';
-
-    canvasProcess.width = 10;
-    canvasProcess.height = 10;
-    contextProcess.drawImage(image, 0, 0, 10, 10);
-    const grayscaleArray = [];
-
-    const imageDataProcess = contextProcess.getImageData(0, 0, 10, 10);
-
-    const pixelsProcess = imageDataProcess.data;
-
-    console.log(pixels);
-    for (let i = 0; i < pixelsProcess.length; i += 4) {
-      let grayscale =
-        (pixelsProcess[i] + pixelsProcess[i + 1] + pixelsProcess[i + 2]) / 3;
-      pixelsProcess[i] = grayscale;
-      pixelsProcess[i + 1] = grayscale;
-      pixelsProcess[i + 2] = grayscale;
-      grayscaleArray.push(Math.floor(grayscale));
-    }
-
-    contextProcess.putImageData(imageDataProcess, 0, 0);
-
+    // Get Table and clear items
     const tableGrayscaleEl = document.querySelector('.table-grayscale');
-
     tableGrayscaleEl.innerHTML = '';
 
-    // Hitung jumlah baris dan kolom tabel
-    const numRows = 10;
-    const numCols = 10;
+    // Total column and row
+    const numRows = 7;
+    const numCols = 7;
 
     let index = 0;
     for (let i = 0; i < numRows; i++) {
@@ -260,10 +134,127 @@ function process() {
       }
     }
 
-    // Show canvas
-    const canvasEl = document.querySelectorAll('.result canvas');
-    canvasEl.forEach(i => {
-      i.style.display = 'block';
-    });
+    // Show Table proses
+    const robertProcessEl = document.querySelector('.robert-process');
+    robertProcessEl.style.display = 'block';
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function process() {
+  processImageData();
+  resultButtonEl.removeAttribute('disabled');
+  processButtonEl.setAttribute('disabled', '');
+}
+
+async function showResult() {
+  resultContainer.innerHTML = '';
+
+  const originalCanvas = document.createElement('canvas');
+  const grayscaleCanvas = document.createElement('canvas');
+  const resultCanvas = document.createElement('canvas');
+
+  const originalContext = originalCanvas.getContext('2d');
+  const grayscaleContext = grayscaleCanvas.getContext('2d');
+  const resultContext = resultCanvas.getContext('2d');
+
+  const minSize = 100;
+  const maxSize = 900;
+
+  const file = inputFileEl.files[0];
+  const reader = new FileReader();
+  const newImage = new Image();
+
+  reader.onload = function () {
+    newImage.onload = function () {
+      const ratio = newImage.width / newImage.height;
+      let width = Math.min(maxSize, Math.max(minSize, originalCanvas.width));
+      let height = width / ratio;
+
+      if (height > maxSize) {
+        height = maxSize;
+        width = height * ratio;
+      }
+
+      originalCanvas.width = width;
+      originalCanvas.height = height;
+
+      originalContext.drawImage(newImage, 0, 0, width, height);
+
+      // Result Grayscale
+      const imageDataGreyScale = originalContext.getImageData(
+        0,
+        0,
+        width,
+        height
+      );
+      const pixels = imageDataGreyScale.data;
+      for (let i = 0; i < pixels.length; i += 4) {
+        let grayscale = (pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3;
+        pixels[i] = grayscale;
+        pixels[i + 1] = grayscale;
+        pixels[i + 2] = grayscale;
+      }
+
+      grayscaleCanvas.width = width;
+      grayscaleCanvas.height = height;
+      grayscaleContext.putImageData(imageDataGreyScale, 0, 0);
+
+      // Result Robert
+      const imageDataRobert = grayscaleContext.getImageData(
+        0,
+        0,
+        width,
+        height
+      );
+      const pixelsRobert = imageDataRobert.data;
+      const output = new Uint8ClampedArray(pixelsRobert.length);
+
+      const kernelGx = [1, 0, 0, -1];
+      const kernelGy = [0, 1, -1, 0];
+
+      for (let i = 0; i < pixelsRobert.length; i += 4) {
+        let gx = 0;
+        let gy = 0;
+
+        for (let j = 0; j < 4; j++) {
+          let x = (i / 4) % grayscaleCanvas.width;
+          let y = Math.floor(i / 4 / grayscaleCanvas.width);
+          let index =
+            ((y + Math.floor(j / 2)) * grayscaleCanvas.width + (x + (j % 2))) *
+            4;
+
+          gx += pixelsRobert[index] * kernelGx[j];
+          gy += pixelsRobert[index] * kernelGy[j];
+        }
+
+        let magnitude = Math.sqrt(gx * gx + gy * gy);
+        output[i] = magnitude;
+        output[i + 1] = magnitude;
+        output[i + 2] = magnitude;
+        output[i + 3] = 255;
+      }
+
+      resultCanvas.width = width;
+      resultCanvas.height = height;
+      imageDataRobert.data.set(output);
+      resultContext.putImageData(imageDataRobert, 0, 0);
+      resultContainer.append(originalCanvas);
+      resultContainer.append(grayscaleCanvas);
+      resultContainer.append(resultCanvas);
+    };
+    if (file) {
+      newImage.src = reader.result;
+    } else {
+      newImage.src = Array.from(document.getElementsByName('image')).find(
+        r => r.checked
+      ).nextElementSibling.src;
+    }
   };
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    reader.readAsDataURL(new Blob());
+  }
 }
