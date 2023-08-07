5b2cfc78238460b36e320cd35c6397903c813686
Rahmat Ardiansyah <rahmatardiansyah147@gmail.com>
2023-08-07 15:53:41 +0700

    menambahakan bagian materi dasar

--- src/components/molekul/Basic/index.jsx
+++ src/components/molekul/Basic/index.jsx
@@ -1,5 +1,105 @@
+import './basic.scss';
+
 const Basic = () => {
-  return <div></div>;
+  return (
+    <div className="container basic-section">
+      <h2>Materi Dasar</h2>
+      <div className="row">
+        <div className="col-xl-6">
+          <div className="d-flex">
+            <div className="number">01</div>
+            <div className="basic-info">
+              <div className="basic-title">Image Enhancement</div>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Quaerat voluptas illo inventore non iusto aliquid tempora
+                molestias similique qui et?
+              </p>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className="number">02</div>
+            <div className="basic-info">
+              <div className="basic-title">Image Enhancement</div>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Quaerat voluptas illo inventore non iusto aliquid tempora
+                molestias similique qui et?
+              </p>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className="number">03</div>
+            <div className="basic-info">
+              <div className="basic-title">Image Restoration </div>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Quaerat voluptas illo inventore non iusto aliquid tempora
+                molestias similique qui et?
+              </p>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className="number">04</div>
+            <div className="basic-info">
+              <div className="basic-title">Pemrosesan Gambar Berwarna</div>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Quaerat voluptas illo inventore non iusto aliquid tempora
+                molestias similique qui et?
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-6">
+          <div className="d-flex">
+            <div className="number">05</div>
+            <div className="basic-info">
+              <div className="basic-title">Multi-Resolution Processing</div>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Quaerat voluptas illo inventore non iusto aliquid tempora
+                molestias similique qui et?
+              </p>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className="number">06</div>
+            <div className="basic-info">
+              <div className="basic-title">Compression</div>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Quaerat voluptas illo inventore non iusto aliquid tempora
+                molestias similique qui et?
+              </p>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className="number">07</div>
+            <div className="basic-info">
+              <div className="basic-title">Morphological Processing</div>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Quaerat voluptas illo inventore non iusto aliquid tempora
+                molestias similique qui et?
+              </p>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className="number">08</div>
+            <div className="basic-info">
+              <div className="basic-title">Segmentation</div>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Quaerat voluptas illo inventore non iusto aliquid tempora
+                molestias similique qui et?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Basic;
