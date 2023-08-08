315f3e45154006875a353b7145229aa563534242
Rahmat Ardiansyah <rahmatardiansyah147@gmail.com>
2023-08-08 17:31:17 +0700

    menambahkan katex

--- src/pages/Robert/index.jsx
+++ src/pages/Robert/index.jsx
@@ -1,5 +1,56 @@
+import 'katex/dist/katex.min.css';
+import { InlineMath } from 'react-katex';
+import './robert.scss';
+
 const Robert = () => {
-  return <h1>Ini halaman Robert</h1>;
+  return (
+    <div className="container robert-section">
+      <h3>Robert</h3>
+      <p>
+        Operator yang berbasis gradien yang menggunakan dua buah kernel yang
+        berukuran 2x2 piksel. Operator ini mengambil arah diagonal untuk
+        penentuan arah dalam penghitungan nilai gradien, sehingga sering disebut
+        dengan operator silang.
+      </p>
+      <div className="row">
+        <div className="col-xl-4">
+          <table className="table table-bordered robert-table text-center">
+            <tbody>
+              <tr>
+                <td className="rm-border-top rm-border-left rm-border-right"></td>
+                <td className="rm-border-top rm-border-left">
+                  <InlineMath>x</InlineMath>
+                </td>
+                <td>x+1</td>
+              </tr>
+              <tr>
+                <td className="rm-border-top rm-border-left">y</td>
+                <td>
+                  <InlineMath>z_1</InlineMath>
+                </td>
+                <td>
+                  <InlineMath>z_2</InlineMath>
+                </td>
+              </tr>
+              <tr>
+                <td className="rm-border-top rm-border-left rm-border-bottom">
+                  <InlineMath>y+1</InlineMath>
+                </td>
+                <td>
+                  <InlineMath>z_3</InlineMath>
+                </td>
+                <td>
+                  <InlineMath>z_1</InlineMath>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="col-xl-4"></div>
+        <div className="col-xl-4"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Robert;
