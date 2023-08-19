216e51d004e622789e4f9c9305783529d13b91b8
Rahmat Ardiansyah <rahmatardiansyah147@gmail.com>
2023-08-19 16:18:33 +0700

    menambahkan bagian robert "icon,image,table,currentprocess"

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,4 +1,9 @@
 import ReactLogo from './icons/react.svg';
 import HeroImage from './images/hero.jpg';
+import Image1 from './images/image1.jpg';
+import Image2 from './images/image2.jpg';
+import Image3 from './images/image3.jpg';
+import Image4 from './images/image4.jpg';
+import Image5 from './images/image5.jpg';
 
-export { ReactLogo, HeroImage };
+export { ReactLogo, HeroImage, Image1, Image2, Image3, Image4, Image5 };
--- src/pages/Robert/index.jsx
+++ src/pages/Robert/index.jsx
@@ -1,19 +1,30 @@
 import { TableRobert, Button, Katex } from '../../components';
 import { useRef } from 'react';
-
+import { Image1, Image2, Image3, Image4, Image5 } from '../../assets';
+import {
+  FaCircleChevronRight,
+  FaCirclePlay,
+  FaCirclePause,
+  FaCircleStop
+} from 'react-icons/fa6';
 import './robert.scss';
 
 const Robert = () => {
   const inputRef = useRef(null);
 
   const handleFileChange = event => {
-    const fileObj = event.target.files && event.target.files[0];
+    const fileObj = event.target.files[0];
     if (!fileObj) {
       return;
     }
   };
   const resetFileInput = () => {
     inputRef.current.value = null;
+  };
+  const process = () => {
+    const menuToggle = document.getElementById('collapseExample');
+    const bsCollapse = new bootstrap.Collapse(menuToggle);
+    bsCollapse.toggle();
   };
   return (
     <div className=" robert-section">
@@ -33,7 +44,15 @@ const Robert = () => {
       <div className="divider my-5"></div>
       <div className="container">
         <div className="d-flex justify-content-evenly flex-wrap gap-3 mb-5">
-          <Button text="Pilih Gambar" className="btn btn-primary" />
+          <Button
+            text="Pilih Gambar"
+            className="btn btn-primary"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseExample"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+          />
           <div className="input-group p-2">
             <input
               type="file"
@@ -53,7 +72,201 @@ const Robert = () => {
               onClick={resetFileInput}
             />
           </div>
-          <Button text="Proses" className="btn btn-success process" />
+          <Button
+            text="Proses"
+            className="btn btn-success process"
+            onClick={process}
+          />
+        </div>
+        <div className="collapse" id="collapseExample">
+          <div className="card card-body ">
+            <div className="hidden-radio d-flex justify-content-evenly flex-wrap">
+              <label>
+                <input
+                  type="radio"
+                  name="image"
+                  className="radio-choose-image"
+                />
+                <img
+                  src={Image1}
+                  className="rounded mx-2 my-2"
+                  alt="Apel"
+                  width="200"
+                />
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="image"
+                  className="radio-choose-image"
+                />
+                <img
+                  src={Image2}
+                  className="rounded mx-2 my-2"
+                  alt="Kok"
+                  width="200"
+                />
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="image"
+                  className="radio-choose-image"
+                />
+                <img
+                  src={Image3}
+                  className="rounded mx-2 my-2"
+                  alt="Daun"
+                  width="200"
+                />
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="image"
+                  className="radio-choose-image"
+                />
+                <img
+                  src={Image4}
+                  className="rounded mx-2 my-2"
+                  alt="Burung Hantu"
+                  width="200"
+                />
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="image"
+                  className="radio-choose-image"
+                />
+                <img
+                  src={Image5}
+                  className="rounded mx-2 my-2"
+                  alt="Jam"
+                  width="200"
+                />
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-evenly flex-wrap gap-4 my-5">
+          <img src={Image1} className="img-fluid" width="300" alt="..." />
+          <img
+            src={Image1}
+            className="img-fluid robert-result"
+            width="300"
+            alt="..."
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          />
+          <img
+            src={Image1}
+            className="img-fluid robert-result"
+            width="300"
+            alt="..."
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          />
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-7">
+            <table className="table table-bordered text-center">
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                  <td>6</td>
+                  <td>7</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                  <td>6</td>
+                  <td>7</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                  <td>6</td>
+                  <td>7</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                  <td>6</td>
+                  <td>7</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                  <td>6</td>
+                  <td>7</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                  <td>6</td>
+                  <td>7</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                  <td>6</td>
+                  <td>7</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="col-xl-5 current-process">
+            <div className="current-process-title">Current Process</div>
+            <Katex mathExpression="\sqrt{(1-2)^2 + (1-2)^2} = 1" />
+            <Katex mathExpression="\sqrt{(1-2)^2 + (1-2)^2} = 1" />
+            <Katex mathExpression="\sqrt{(1-2)^2 + (1-2)^2} = 1" />
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="d-flex justify-content-evenly flex-wrap my-5 animation-button">
+          <FaCircleChevronRight size={80} />
+          <FaCirclePlay size={80} />
+          <FaCirclePause size={80} />
+          <FaCircleStop size={80} />
+        </div>
+      </div>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content p-2">
+            <img src={Image5} alt="" />
+          </div>
         </div>
       </div>
     </div>
