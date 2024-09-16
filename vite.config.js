427507a4c5348683fd379d20abcdc24e2cc430b4
Rahmat Ardiansyah <artsbymat@gmail.com>
2024-09-16 20:12:07 +0700

    feat: add constant variable version

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://rahmatardiansyah.github.io/vip'
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
+  },
+  base: process.env.NODE_ENV === 'production' ? 'https://rahmatardiansyah.github.io/vip' : '/'
 });
